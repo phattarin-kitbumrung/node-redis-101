bbddee755a9f1d53f0de8960229f4d96ad021d63
phattarin kitbumrung <phattarin.kitbumrung@gmail.com>
2021-01-02 16:11:43 +0700

    first commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# node-redis-101
+
+Node Redis Example : บทความ https://devahoy.com/blog/2019/11/how-to-caching-node-api-with-redis/
---
+++ app.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const axios = require('axios');
+const redis = require('redis');
+
+const app = express();
+const redisClient = redis.createClient();
+
+app.get('/', (req, res) => {
+  const username = req.query.username || 'devahoy';
+
+  const url = `https://api.github.com/users/${username}`;
+
+  redisClient.get(username, async (err, reply) => {
+    if (reply) {
+      console.log('data from redis')
+      return res.json(JSON.parse(reply));
+    }
+
+    const response = await axios.get(url);
+    redisClient.setex(username, 60, JSON.stringify(response.data));
+    console.log('data from axios')
+    return res.json(response.data);
+  });
+});
+
+app.listen(9000, () => {
+  console.log('running');
+});
